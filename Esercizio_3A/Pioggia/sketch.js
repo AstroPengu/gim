289dba558876bc9a119194a5a2667bd37667308e
Sophie Jean-Richard <sophiejeanrichard3@gmail.com>
2025-06-15 18:24:09 +0200

    Update sketch.js

--- Esercizio_3A/Pioggia/sketch.js
+++ Esercizio_3A/Pioggia/sketch.js
@@ -8,17 +8,22 @@ function windowResized() {
 
 function draw() {
     clear(); // Sfondo trasparente - così si vede l'immagine HTML dietro
-    
-    let dimensione = 300;
 
-    for(let i=0; i<100; i++) {
-        let gocciaLunghezza = random(10, 150);
-        let gocciaX = random(-dimensione, dimensione);
-        let gocciaY = random(-dimensione, dimensione, -gocciaLunghezza);
-        let gocciaZ = random(-dimensione, dimensione);
+    // Calcola le dimensioni basate sulla finestra
+    let larghezza = windowWidth / 2;
+    let altezza = windowHeight / 2;
+    let profondita = 300; // Puoi mantenere fisso questo valore per la profondità
+
+    for(let i = 0; i < 150; i++) { // Aumentato il numero di gocce
+        let gocciaLunghezza = random(10, 100);
         
-        strokeWeight(random (1, 2));
-        stroke(255, 255, 255, 150); // Bianco con trasparenza (150 su 255)
+        // Distribuzione su tutta la larghezza e altezza della finestra
+        let gocciaX = random(-larghezza, larghezza);
+        let gocciaY = random(-altezza, altezza - gocciaLunghezza);
+        let gocciaZ = random(-profondita, profondita);
+
+        strokeWeight(random(1, 2));
+        stroke(255, 255, 255, 150); // Bianco con trasparenza
         line(gocciaX, gocciaY, gocciaZ, gocciaX, gocciaY + gocciaLunghezza, gocciaZ);
     }
 }
